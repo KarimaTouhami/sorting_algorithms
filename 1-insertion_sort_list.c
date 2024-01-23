6bf447f7fc71784dc9df1756d5460d3d01898206
Karima Touhami <113696378+KarimaTouhami@users.noreply.github.com>
2024-01-23 16:56:23 +0000

    fixall betty

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,43 +1,39 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of integers
- * in ascending order using the Insertion sort algorithm
- * @list: doubly linked list of integers
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *                       in ascending order using the Insertion sort algorithm
  *
- * Return: void
+ * @list: The doubly linked list to be sorted
  */
 
 void insertion_sort_list(listint_t **list)
 {
     listint_t *current, *next, *prev;
 
-    if (list == NULL || *list == NULL)
+    if (!list || !(*list) || !(*list)->next)
         return;
 
-    current = *list;
-    next = current->next;
-
-    while (next != NULL)
+    current = (*list)->next;
+    while (current)
     {
-        prev = current;
         next = current->next;
-        if (current->n > next->n)
+        prev = current->prev;
+        while (prev && prev->n > current->n)
         {
-            if (current->prev != NULL)
-                current->prev->next = next;
-            else
-                *list = next;
-            current->next = next->next;
-            next->prev = current->prev;
-            current->prev = next;
-            next->next = current;
-            if (current->next != NULL)
-                current->next->prev = current;
+            prev->next = current->next;
+            if (current->next)
+                current->next->prev = prev;
+            current->next = prev;
+            current->prev = prev->prev;
+            if (prev->prev)
+                prev->prev->next = current;
+            prev->prev = current;
+            prev = current->prev;
+            if (!current->prev)
+                *list = current;
             print_list(*list);
-            current = prev;
         }
-        else
-            current = current->next;
+        current = next;
     }
 }
